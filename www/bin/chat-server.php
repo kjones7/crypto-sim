41dd82946bfaf297836bab462d72ee5deb6f715a
Kyle Jones <mynameiskyle242@gmail.com>
2018-06-10 18:02:19 -0400

    Added script that runs websocket connection

---
+++ www/bin/chat-server.php
@@ -0,0 +1,30 @@
+<?php
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+use MyApp\Chat;
+
+    require dirname(__DIR__) . '/vendor/autoload.php';
+
+    $server = IoServer::factory(
+        new HttpServer(
+            new WsServer(
+                new Chat()
+            )
+        ),
+        8079
+    );
+
+    $server->run();
+
+    // $chatServer = new Chat();
+    // $port = 8079;
+    // $ip='0.0.0.0';
+
+    // $wsServer = new WsServer($chatServer);
+    // // $wsServer->disableVersion(0); // old, bad, protocol version
+            
+    // $http = new HttpServer($wsServer);
+    // $server = IoServer::factory($http, $port, $ip);
+
+    // $server->run();
