cdb2df4b50189801c5d0e85baacfb6e49bd9263e
Kyle Jones <mynameiskyle242@gmail.com>
2018-06-19 22:43:51 -0400

    Added changes to DatabaseController

--- www/src/controllers/DatabaseController.php
+++ www/src/controllers/DatabaseController.php
@@ -9,20 +9,20 @@ require "/var/www/html/vendor/autoload.php";
 
 class DatabaseController {
     private $database;
+    private $conn;
 
     function __construct(){
         $this->database = new Database();
+        $this->conn = $this->database->getConn();
     }
 
     public function createAccount(User $user) : void {
-        /* @var PDO */
-        $conn = $this->database->getConn();
         $email = $user->getEmail();
         $username = $user->getUsername();
         $password = $user->getPassword();
         $country = $user->getCountry();
 
-        $stmt = $conn->prepare(
+        $stmt = $this->conn->prepare(
             "INSERT INTO users(email, username, password, country, date_created)"
         .   "VALUES(:email, :username, :password, :country, NOW())"
         );
@@ -35,5 +35,18 @@ class DatabaseController {
         if(!$stmt->execute()) {
             echo "Error: " . $stmt->errorInfo()[2];
         }
+    }
+
+    public function sendFriendRequest($fromUsername, $toUsername) {
+        $stmt = $this->conn->prepare(
+            "UPDATE"
+        );
+    }
+
+    private function validateFriendRequest($fromUsername, $toUsername) {
+        $stmt = $this->conn->prepare(
+            "SELECT EXISTS(SELECT * FROM friend_requests WHERE to_user_id = :toUserId AND from_user_id = :fromUserId)"
+        );
+        $stmt->bindParam(':toUserId', $toUser);
     }
 }
