d2e2dededcd93e8873149a1ca3b9c207e5162c95
Kyle Jones <mynameiskyle242@gmail.com>
2018-08-04 23:34:38 -0400

    On first page load of simulation page for a portfolio, the portfolios transactions are used to display the dollar amount, crypto worth in dollars, and portfolio worth

--- www/src/Simulation/Application/Portfolio.php
+++ www/src/Simulation/Application/Portfolio.php
@@ -6,13 +6,22 @@ final class Portfolio
 {
     private $id;
     private $title;
+    private $USDAmount;
+    private $cryptoWorthInUSD;
+    private $portfolioWorth;
 
     public function __construct(
         string $id,
-        string $title
+        string $title,
+        string $USDAmount,
+        string $cryptoWorthInUSD,
+        string $portfolioWorth
     ){
         $this->id = $id;
         $this->title = $title;
+        $this->USDAmount = $USDAmount;
+        $this->cryptoWorthInUSD = $cryptoWorthInUSD;
+        $this->portfolioWorth = $portfolioWorth;
     }
     /**
      * @return string
@@ -28,5 +37,29 @@ final class Portfolio
     public function getId(): string
     {
         return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUSDAmount(): string
+    {
+        return $this->USDAmount;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCryptoWorthInUSD(): string
+    {
+        return $this->cryptoWorthInUSD;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPortfolioWorth(): string
+    {
+        return $this->portfolioWorth;
     }
 }
--- www/src/Simulation/Infrastructure/DbalPortfolioRepository.php
+++ www/src/Simulation/Infrastructure/DbalPortfolioRepository.php
@@ -32,9 +32,73 @@ final class DbalPortfolioRepository implements PortfolioRepository
             return null;
         }
 
+        $portfolioUSDAmount = $this->getPortfolioUSDAmountFromId($portfolioId);
+        $portfolioCryptoWorthInUSD = $this->getPortfolioCryptoWorthInUSDFromId($portfolioId);
+        $portfolioWorth = (string)($portfolioUSDAmount + $portfolioCryptoWorthInUSD);
+
         return new Portfolio(
             $row['id'],
-            $row['title']
+            $row['title'],
+            $portfolioUSDAmount,
+            $portfolioCryptoWorthInUSD,
+            $portfolioWorth
         );
     }
+
+    private function getPortfolioCryptoWorthInUSDFromId(string $portfolioId): ?string
+    {
+//        $qb = $this->connection->createQueryBuilder();
+        $stmt = $this->connection->prepare(
+            "SELECT (
+                        (SELECT SUM(t.usd_amount) FROM transactions t WHERE type = 'buy' AND t.portfolio_id = :portfolioId1 AND status='active')
+                         - (SELECT SUM(t.usd_amount) FROM transactions t WHERE type = 'sell' AND t.portfolio_id = :portfolioId2 AND status='active')) 
+                         AS crypto_amount
+         ");
+        $stmt->bindParam(':portfolioId1', $portfolioId);
+        $stmt->bindParam(':portfolioId2', $portfolioId);
+        $stmt->execute();
+
+        $row =$stmt->fetch();
+
+        if(!$row) {
+            return null; // TODO - Add error catching for when this returns NULL
+        }
+
+        return $row['crypto_amount'];
+    }
+
+
+    private function getPortfolioUSDAmountFromId(string $portfolioId): ?string
+    {
+        // TODO - Make these queries readable
+        $stmt = $this->connection->prepare(
+            "SELECT (
+                        portfolios.start_amount -
+                        (
+                          (SELECT SUM(t.usd_amount) FROM transactions t WHERE type = 'buy' AND t.portfolio_id = :portfolioId1 AND status='active')
+                        - (SELECT SUM(t.usd_amount) FROM transactions t WHERE type = 'sell' AND t.portfolio_id = :portfolioId2 AND status='active')
+                        )
+                     ) AS usd_amount
+                     FROM portfolios"
+        );
+        $stmt->bindParam(':portfolioId1', $portfolioId);
+        $stmt->bindParam(':portfolioId2', $portfolioId);
+        $stmt->execute();
+
+        $row =$stmt->fetch();
+
+        if(!$row) {
+            return null; // TODO - Add error catching for when this returns NULL
+        }
+
+        return $row['usd_amount'];
+    }
+
+//    private function getPortfolioWorth(string $portfolioId): string
+//    {
+//        $USDAmount = $this->getPortfolioUSDAmountFromId($portfolioId);
+//        $cryptoAmountInUSD = $this->getPortfolioCryptoAmountInUSDFromId($portfolioId);
+//
+//        return $USDAmount + $cryptoAmountInUSD;
+//    }
 }
