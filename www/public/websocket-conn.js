c8c14e7dc5dac3fe92143e5bac05c86f8987f0b0
Kyle Jones <mynameiskyle242@gmail.com>
2018-08-08 23:40:28 -0400

    Removed form to be dynamically added, since it causes the browser to freeze

--- www/public/websocket-conn.js
+++ www/public/websocket-conn.js
@@ -7,13 +7,13 @@ var conn = new ab.Session('ws://localhost:8079',
 
             for(var symbol in cryptoData) {
                 let div = `
-                    <form action="" method="POST">
+                    <!--<form action="" method="POST">-->
                         <div id="${symbol}" style="padding-bottom: 5px">
                             <span class="crypto">${symbol} - $${cryptoData[symbol]}</span>
                             <input type="text" name="transaction-amount">
                             <button type="submit" class="buy-crypto" value="${cryptoData[symbol]}">Buy</button>
                         </div>
-                    </form>
+                    <!--</form>-->
                 `;
                 cryptoDataElement.innerHTML += div;
             }
