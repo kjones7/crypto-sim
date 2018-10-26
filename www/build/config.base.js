974ff7e76923768bbe3e45b407c0c6bebd07fb20
Kyle Jones <mynameiskyle242@gmail.com>
2018-10-26 19:28:45 -0400

    Simulation page loads, currently not working
    A lot of code needs to be refactored now that imports needs to be used

--- www/build/config.base.js
+++ www/build/config.base.js
@@ -1,18 +1,16 @@
 const path = require('path');
 const { SRC, DIST, ASSETS } = require('./paths');
 require("babel-register");
+require("babel-polyfill");
 
 module.exports = {
     entry: {
-        scripts: path.resolve(SRC, 'js', 'index.js')
+        simulation: ['babel-polyfill', path.resolve(SRC, 'js', 'Controllers', 'Simulation', 'SimulationController.js')]
     },
     output: {
         // Put all the bundled stuff in your dist folder
         path: DIST,
-
-        // Our single entry point from above will be named "scripts.js"
         filename: '[name].js',
-
         // The output path as seen from the domain we're visiting in the browser
         publicPath: ASSETS
     },
