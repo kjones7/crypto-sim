e880707e353f3be9927198db92ea8028007ba3b8
Kyle Jones <mynameiskyle242@gmail.com>
2019-01-15 19:55:57 -0500

    Add documentation to some CSRF files

--- www/src/Framework/Csrf/SymfonySessionTokenStorage.php
+++ www/src/Framework/Csrf/SymfonySessionTokenStorage.php
@@ -4,20 +4,39 @@ namespace CryptoSim\Framework\Csrf;
 
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
+/**
+ * Token storage using Symfony's session
+ * @package CryptoSim\Framework\Csrf
+ */
 final class SymfonySessionTokenStorage implements TokenStorage
 {
+    /** @var SessionInterface Session of the current user */
     private $session;
 
+    /**
+     * SymfonySessionTokenStorage constructor.
+     * @param SessionInterface $session
+     */
     public function __construct(SessionInterface $session)
     {
         $this->session = $session;
     }
 
+    /**
+     * Stores the CSRF token in the session using a key
+     * @param string $key The key to be used to store/access the CSRF token
+     * @param Token $token The CSRF token
+     */
     public function store(string $key, Token $token): void
     {
         $this->session->set($key, $token->toString());
     }
 
+    /**
+     * Retrieves the CSRF token from the current user's session
+     * @param string $key The key used to access the CSRF token in the session
+     * @return Token|null Returns token if found, null if not found
+     */
     public function retrieve(string $key): ?Token
     {
         $tokenValue = $this->session->get($key);
--- www/src/Framework/Csrf/Token.php
+++ www/src/Framework/Csrf/Token.php
@@ -2,27 +2,49 @@
 
 namespace CryptoSim\Framework\Csrf;
 
+/**
+ * Represents a CSRF token
+ *
+ * @package CryptoSim\Framework\Csrf
+ */
 final class Token
 {
+    /** @var string The CSRF token */
     private $token;
 
-    // TODO - should this function be private, since we have a static function
+    /**
+     * Token constructor.
+     * @param string $token
+     */
     public function __construct(string $token)
     {
         $this->token = $token;
     }
 
+    /**
+     * @return string The CSRF token as a string
+     */
     public function toString(): string
     {
         return $this->token;
     }
 
+    /**
+     * Generates a CSRF token
+     * @return Token The CSRF token
+     * @throws \Exception
+     */
     public static function generate(): Token
     {
         $token = bin2hex(random_bytes(256));
         return new Token($token);
     }
 
+    /**
+     * Checks if two tokens are equal to each other
+     * @param Token $token A token to check equality with
+     * @return bool
+     */
     public function equals(Token $token): bool
     {
         return ($this->token === $token->toString());
