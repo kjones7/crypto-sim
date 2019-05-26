90d1e9d98659f9fd5aa478db0afe6d3925f5c507
Kyle Jones <mynameiskyle242@gmail.com>
2019-05-25 23:38:52 -0400

    Remove references of old DB password from db creation script

--- www/sql/createDBs.sh
+++ www/sql/createDBs.sh
@@ -2,13 +2,13 @@
 # TODO - Remove all references to database password/usernames
 
 # Create main database, create tables, create empty test database
-mysql -u root -ptiger -h tnb-mysql < sqlCreateDBs.sql
+mysql -u root -p${DB_ROOT_PASSWORD} -h tnb-mysql < sqlCreateDBs.sql
 
 # Dump main database
-mysqldump -u root -ptiger -h tnb-mysql crypto_sim > dumps/crypto_sim-dump.sql
+mysqldump -u root -p${DB_ROOT_PASSWORD} -h tnb-mysql crypto_sim > dumps/crypto_sim-dump.sql
 
 # Copy main database to create test database using dump
-mysql -u root -ptiger -h tnb-mysql crypto_sim_test < dumps/crypto_sim-dump.sql
+mysql -u root -p${DB_ROOT_PASSWORD} -h tnb-mysql crypto_sim_test < dumps/crypto_sim-dump.sql
 
 # Create dump of test database to be used by testing framework to repopulate
-mysqldump -u root -ptiger -h tnb-mysql crypto_sim_test > dumps/crypto_sim_test-dump.sql
+mysqldump -u root -p${DB_ROOT_PASSWORD} -h tnb-mysql crypto_sim_test > dumps/crypto_sim_test-dump.sql
