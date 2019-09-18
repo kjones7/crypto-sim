b1463fce657f53fee799e25cdeb633fcfcf20e27
Kyle Jones <mynameiskyle242@gmail.com>
2019-09-17 21:51:00 -0400

    Make the following changes:
    - Display errors on page when sending friend request fails
    - Refactor SendFriendRequestControllerTest to increase code reuse
    - Add test to ensure errors occur when invalid friend request is sent

--- www/src/User/Presentation/SendFriendRequestController.php
+++ www/src/User/Presentation/SendFriendRequestController.php
@@ -6,24 +6,38 @@ use CryptoSim\User\Application\SendFriendRequest;
 use CryptoSim\User\Application\SendFriendRequestHandler;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 final class SendFriendRequestController
 {
     private $sendFriendRequestHandler;
+    private $session;
 
-    public function __construct(SendFriendRequestHandler $sendFriendRequestHandler)
-    {
+    public function __construct(
+        SendFriendRequestHandler $sendFriendRequestHandler,
+        SessionInterface $session
+    ) {
         $this->sendFriendRequestHandler = $sendFriendRequestHandler;
+        $this->session = $session;
     }
 
     public function send(Request $request) {
         $nickname = $request->get('send-friend-request');
         $userId = $request->get('userId');
 
-        $this->sendFriendRequestHandler->handle(new SendFriendRequest(
-            $nickname,
-            $userId
-        ));
+        try {
+            $this->sendFriendRequestHandler->handle(new SendFriendRequest(
+                $nickname,
+                $userId
+            ));
+        } catch (\Exception $e) {
+            $this->session->getFlashBag()->add(
+                'errors',
+                'An error occurred while sending the friend request. Please try again.'
+            );
+        }
+
+
 
         return new RedirectResponse("/user/{$nickname}");
     }
--- www/tests/integration/User/Presentation/SendFriendRequestControllerTest.php
+++ www/tests/integration/User/Presentation/SendFriendRequestControllerTest.php
@@ -17,8 +17,27 @@ class SendFriendRequestControllerTest extends Unit
      */
     protected $tester;
 
+    private $connection;
+    private $user1;
+    private $user2;
+    private $user1Session;
+    private $user2Session;
+
     protected function _before()
     {
+        // TODO - This doesn't need to be run before each test, the users could just be added once
+        // Setup database connection
+        $this->connection = $this->tester->createDatabaseConnection();
+
+        // Create users and sessions for those users
+        $this->user1 = $this->tester->createRandomUser();
+        $this->user2 = $this->tester->createRandomUser();
+        $this->user1Session = $this->tester->setupSessionForUser($this->user1);
+        $this->user2Session = $this->tester->setupSessionForUser($this->user2);
+
+        // Add users to database
+        $this->tester->addUserToDatabase($this->user1, $this->user1Session);
+        $this->tester->addUsertoDatabase($this->user2, $this->user2Session);
     }
 
     protected function _after()
@@ -27,32 +46,18 @@ class SendFriendRequestControllerTest extends Unit
 
     public function testSendingFriendRequest()
     {
-        // Setup database connection
-        $connection = $this->tester->createDatabaseConnection();
-
-        // Create users and sessions for those users
-        $user1 = $this->tester->createRandomUser();
-        $user2 = $this->tester->createRandomUser();
-        $session1 = $this->tester->setupSessionForUser($user1);
-        $session2 = $this->tester->setupSessionForUser($user2);
-
-        // Add users to database
-        $this->tester->addUserToDatabase($user1, $session1);
-        $this->tester->addUsertoDatabase($user2, $session2);
-
-
-        $friendRequestRepository = new DbalFriendRequestsRepository($connection, $session2);
+        $friendRequestRepository = new DbalFriendRequestsRepository($this->connection, $this->user2Session);
         $sendFriendRequestHandler = new SendFriendRequestHandler($friendRequestRepository);
-        $sendFriendRequestController = new SendFriendRequestController($sendFriendRequestHandler);
+        $sendFriendRequestController = new SendFriendRequestController($sendFriendRequestHandler, $this->user2Session);
 
         $mockPOSTData = [
-            'send-friend-request' => $user1->getNickname(),
-            'userId' => $user1->getId()->toString(),
+            'send-friend-request' => $this->user1->getNickname(),
+            'userId' => $this->user1->getId()->toString(),
         ];
 
         // TODO - When creating request, $_COOKIE should be set with the ID of the session (PHPSESSID cookie)
 //        $mockCOOKIEData = [
-//            'PHPSESSID' => $session2->get('userId'),
+//            'PHPSESSID' => $this->user2Session->get('userId'),
 //        ];
 
         $_POST = $mockPOSTData;
@@ -64,17 +69,40 @@ class SendFriendRequestControllerTest extends Unit
         $sendFriendRequestController->send($request);
 
         // Check that user1 received friend request from user2
-        $dbalFriendRequestsQuery = new DbalFriendRequestsQuery($connection, $session1);
+        $dbalFriendRequestsQuery = new DbalFriendRequestsQuery($this->connection, $this->user1Session);
         $friendRequests = $dbalFriendRequestsQuery->execute();
 
         $expectedFriendRequests = [
             new FriendRequest(
-                $user2->getNickname(),
-                $user2->getId()
+                $this->user2->getNickname(),
+                $this->user2->getId()
             )
         ];
 
         $this->tester->assertEquals($expectedFriendRequests, $friendRequests);
 
+    }
+
+    public function testShould_HaveErrors_When_toUserIDIsInvalid()
+    {
+        // $user2 sends a friend request to a user ID that doesn't exist
+        $friendRequestRepository = new DbalFriendRequestsRepository($this->connection, $this->user2Session);
+        $sendFriendRequestHandler = new SendFriendRequestHandler($friendRequestRepository);
+        $sendFriendRequestController = new SendFriendRequestController($sendFriendRequestHandler, $this->user2Session);
+
+        $mockPOSTData = [
+            'send-friend-request' => $this->user1->getNickname(),
+            'userId' => '12345', // user ID does not exist
+        ];
+
+        $_POST = $mockPOSTData;
+
+        $request = Request::createFromGlobals();
+
+        // Send friend request from user2 to user1 using SendFriendRequestController
+        $sendFriendRequestController->send($request);
+
+        // Flash messages should contain error messages
+        $this->tester->assertTrue($this->user2Session->getFlashBag()->has('errors'));
     }
 }
